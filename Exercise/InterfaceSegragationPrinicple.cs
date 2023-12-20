2dd5c480ed07327f713c17e806a84bf5b9314650
ngandolh <94061934+ngandolh@users.noreply.github.com>
2023-12-20 20:32:07 +0700

    SOILD Principle

---
+++ Exercise/InterfaceSegragationPrinicple.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public interface IReadableDocument
+    {
+        void Open();
+    }
+
+    public interface IPrintableDocument
+    {
+        void Print();
+    }
+
+    public interface ISaveableDocument
+    {
+        void Save();
+    }
+
+    public interface IEmailableDocument
+    {
+        void Email();
+    }
+
+    //Classes implementing specific interfaces
+    public class SimpleDocument : IReadableDocument, IPrintableDocument, IEmailableDocument
+    {
+        public void Email()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    //Client using the specific interfaces
+    public class DocumentClient
+    {
+        private IReadableDocument readbleDocument;
+        private IPrintableDocument printableDocument;
+        private ISaveableDocument saveableDocument;
+
+        public DocumentClient(IReadableDocument readDoc, IPrintableDocument printDoc, ISaveableDocument saveDoc)
+        {
+            this.readbleDocument = readDoc;
+            this.printableDocument = printDoc;
+            this.saveableDocument = saveDoc;
+        }
+
+        public void PerformActions()
+        {
+            readbleDocument.Open();
+            printableDocument.Print();
+            saveableDocument.Save();
+        }
+    }
+}
---
+++ Exercise/SingleResponsibility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// Lớp trừu tượng Employee
+public abstract class Employee
+{
+    public abstract void Working();
+}
+
+// Lớp Developer kế thừa từ Employee và triển khai phương thức Working
+public class Developer : Employee
+{
+    public override void Working()
+    {
+        Console.WriteLine("Developer is coding...");
+    }
+}
+
+// Lớp Tester kế thừa từ Employee và triển khai phương thức Working
+public class Tester : Employee
+{
+    public override void Working()
+    {
+        Console.WriteLine("Tester is testing...");
+    }
+}
+
+// Lớp Salesman kế thừa từ Employee và triển khai phương thức Working
+public class Salesman : Employee
+{
+    public override void Working()
+    {
+        Console.WriteLine("Salesman is selling...");
+    }
+}
+
+//class SingleResponsibility
+//{
+//    static void Main(string[] args)
+//    {
+//        Employee dev = new Developer();
+//        Employee tester = new Tester();
+//        Employee salesman = new Salesman();
+
+//        dev.Working();      // Output: Developer is coding...
+//        tester.Working();   // Output: Tester is testing...
+//        salesman.Working(); // Output: Salesman is selling...
+//    }
+//}
