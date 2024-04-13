f2bee19103e26f6928f01efa3ba1edafa79370fc
ngandolh <nganttk002@gmail.com>
2024-04-14 02:36:29 +0700

    OOP

---
+++ OOP/Abstraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Abstraction
+    {
+        public abstract class Shape
+        {
+            // Abstract method to calculate area
+            public abstract double CalculateArea();
+        }
+
+        // Concrete class representing a circle
+        public class Circle : Shape
+        {
+            private double radius;
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+
+            // Implementation of CalculateArea method for Circle
+            public override double CalculateArea()
+            {
+                return Math.PI * radius * radius;
+            }
+        }
+
+        //class Program
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        Circle circle = new Circle(5);
+        //        Console.WriteLine("Area of Circle: " + circle.CalculateArea());
+        //    }
+        //}
+    }
+}
---
+++ OOP/Inheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("Some sound");
+        }
+    }
+
+    // Derived class inheriting from Animal
+    public class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Top-level new instance
+        //    Animal animal = new Dog();
+        //    animal.Sound(); // Output: Bark
+        //    Animal animal2 = new Animal();
+        //    animal2.Sound(); //Some sound
+        //}
+    }
+}
+
