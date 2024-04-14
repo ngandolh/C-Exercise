93d4c63e40fc1aaff957ec19167d5d269133b4c6
ngandolh <nganttk002@gmail.com>
2024-04-14 22:34:16 +0700

    TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 3, 2, 3 };
+        Solution solution = new Solution();
+        solution.TwoSum(arr, 6);
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int sum = 0;
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            for (int y = i + 1; y < nums.Length; y++)
+            {
+                if (nums[i] + nums[y] == target)
+                {
+                    return new int[] { i, y };
+                }
+                return new int[] { };
+            }
+        }
+        return new int[] { };
+    }
+}
