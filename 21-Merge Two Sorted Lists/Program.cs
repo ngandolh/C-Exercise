d7caf9808ae1bc1c2fd9cc8325b3a3869437d446
ngandolh <nganttk002@gmail.com>
2024-12-21 22:18:44 +0700

    [Done] 21-Merge Two Sorted Lists

---
+++ 21-Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _21_Merge_Two_Sorted_Lists
+{
+    //Input: list1 = [1,2,4], list2 = [1, 3, 4]
+    //Output: [1, 1, 2, 3, 4, 4]
+    //Example 2:
+
+    //Input: list1 = [], list2 = []
+    //Output: []
+    //Example 3:
+
+    //Input: list1 = [], list2 = [0]
+    //Output: [0]
+    public class MergeTwoLisst
+    {
+        //public static string MergeTwoLisst(ListNode* list1, ListNode* list2)
+        //{
+
+        //}
+
+        
+    }
+
+    public class ListNode
+    {
+        public int Data;
+        public ListNode Next;
+        public ListNode(int x)
+        {
+            this.Data = x;
+            this.Next = null;
+        }
+    }
+
+    //public class NodeDemo
+    //{
+    //    public int Data;
+    //    public int Next;
+    //}
+}
---
+++ Algorithm/QuickSort/QuickSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.QuickSort
+{
+    internal class QuickSort
+    {
+        public static void QuickSortAlgorithm(int[] arr, int left, int right)
+        {
+            //How to find the median pivot in array.
+            int pivot = (int) Math.Ceiling(arr.Length/2.0);
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i] < arr[pivot])
+                {
+                    if (arr[i] > arr[right])
+                    {
+                        swap(arr[i], arr[right]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int i, int j)
+        {
+            int temp = i;
+            i = j;
+            temp = j;
+        }
+
+        static void Main()
+        {
+            int[] arry = { 52, 14, 67, 71, 42, 38, 39, 40, 96 };
+        }
+    }
+}
