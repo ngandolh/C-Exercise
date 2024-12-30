58ce2c09b92f664a7315f3a74ed995c3ad41e49e
ngandolh <nganttk002@gmail.com>
2024-12-30 12:06:23 +0700

    [DONE]

--- 20-ValidParentheses/Program.cs
+++ 20-ValidParentheses/Program.cs
@@ -1,30 +1,79 @@
-﻿namespace _20_ValidParentheses
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20_ValidParentheses
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Example usage
+            Console.WriteLine(IsValid("()"));       // Output: true
+            Console.WriteLine(IsValid("()[]{}"));   // Output: true
+            Console.WriteLine(IsValid("(]"));       // Output: false
+            Console.WriteLine(IsValid("([])"));     // Output: true
         }
-    }
 
-    public bool ValidParentheses(string s)
-    {
-        List<string> Data = new List<string> { ")", "(", "{", "}", "[", "]" };
-        bool result = false;
-        int count = 0;
-        foreach (var item in Data)
+        public static bool IsValid(string s)
         {
-            if (item.Contains(s))
+            // Stack to keep track of opening brackets
+            Stack<char> stack = new Stack<char>();
+
+            // Dictionary to map closing brackets to their corresponding opening brackets
+            Dictionary<char, char> bracketMap = new Dictionary<char, char>()
             {
-                result = true;
-                count++;
-            }
-            else
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' }
+            };
+
+            foreach (char c in s)
             {
-                result = false;
+                if (bracketMap.ContainsKey(c))
+                {
+                    char topElement = stack.Count > 0 ? stack.Pop() : '#';
+
+                    if (topElement != bracketMap[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
             }
+
+            // If the stack is empty, all brackets were matched; otherwise, return false
+            return stack.Count == 0;
         }
-        return result;
+
+        //Top 1: 0ms
+        //public bool IsValid(string s)
+        //{
+        //    char[] stack = new char[s.Length];
+        //    int top = -1;
+
+        //    foreach (char c in s)
+        //    {
+        //        // Öffnende Klammern werden "gepusht"
+        //        if (c == '(' || c == '{' || c == '[')
+        //        {
+        //            stack[++top] = c;
+        //        }
+        //        // Schließende Klammern werden geprüft
+        //        else
+        //        {
+        //            if (top == -1 ||
+        //               (c == ')' && stack[top] != '(') ||
+        //               (c == '}' && stack[top] != '{') ||
+        //               (c == ']' && stack[top] != '['))
+        //            {
+        //                return false;
+        //            }
+        //            top--; // "Pop"
+        //        }
+        //    }
+
+        //}
     }
-}
