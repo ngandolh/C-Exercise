6c493cc7136666a68a6b616935e0f769a24771d1
ngandolh <nganttk002@gmail.com>
2025-01-02 13:43:23 +0700

    [Progress] Initialize

---
+++ 23-MergekSortedLists/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _23_MergekSortedLists
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
