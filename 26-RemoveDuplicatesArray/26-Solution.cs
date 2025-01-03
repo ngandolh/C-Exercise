026b4c019c2a0d93965cca80499366ad11832f06
ngandolh <nganttk002@gmail.com>
2025-01-03 11:35:04 +0700

    [Progress] Bug

--- 26-RemoveDuplicatesArray/26-Solution.cs
+++ 26-RemoveDuplicatesArray/26-Solution.cs
@@ -2,13 +2,18 @@
 
 namespace _26_RemoveDuplicatesArray
 {
-    internal class Program
+    internal class Solution
     {
         static void Main(string[] args)
         {
             int[] nums = { 0, 0, 1, 1, 1, 1, 2, 2, 3, 4, 5 };
             var result = RemoveDuplicates(nums);
-            Console.WriteLine(result);
+
+            int[] nums2 = { 1, 1, 2 };
+            var result2 = RemoveDuplicates(nums2);
+
+            Console.WriteLine(string.Join(", ", result));
+            Console.WriteLine(string.Join(", ", result2));
         }
 
         public static int[] RemoveDuplicates(int[] nums)
@@ -18,10 +23,10 @@ namespace _26_RemoveDuplicatesArray
 
             Array.Sort(nums);
 
-            List<int> list = new List<int>(nums);
-            for (int i = 0; i < nums.Length; i++)
+            List<int> list = new List<int>();
+            for (int i = 1; i < nums.Length; i++)
             {
-                if(nums[i] != nums[i - 1])
+                if (nums[i] != nums[i - 1])
                 {
                     list.Add(nums[i]);
                 }
