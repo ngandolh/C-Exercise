4cf1350f053e7f90c48ef1ea669d722b7f89dfa5
ngandolh <nganttk002@gmail.com>
2025-01-04 21:28:08 +0700

    [Progress] Create solution 27

---
+++ 27-RemoveElement/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _27_RemoveElement
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 3, 2, 2, 3 };
+            int value1 = 2;
+
+        }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+
+        }
+    }
+}
