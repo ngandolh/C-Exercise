abce9d45bee66bbf321b4efa107c848231b31864
ngandolh <nganttk002@gmail.com>
2025-01-06 20:25:13 +0700

    [Progress] Set up

---
+++ 35-SearchInsertPosition/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace _35_SearchInsertPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
