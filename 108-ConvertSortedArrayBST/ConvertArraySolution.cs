e0e0a72a81bc09febd752666bc6a2f99203d874e
ngandolh <nganttk002@gmail.com>
2025-02-18 22:01:28 +0700

    [Done]

---
+++ 108-ConvertSortedArrayBST/ConvertArraySolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _108_ConvertSortedArrayBST
+{
+    public class ConvertArraySolution
+    {
+        public TreeNode SortedArrayToBST(int[] nums)
+        {
+            TreeNode result = BuildBST(nums, 0, nums.Length - 1);
+            return result;
+        }
+
+        private TreeNode BuildBST(int[] nums, int left, int right)
+        {
+            if (left > right) return null;
+
+            int mid = left + (right - left) / 2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = BuildBST(nums, left, mid - 1);
+            root.right = BuildBST(nums, mid + 1, right);
+
+            return root;
+        }
+
+        public void PrintInOrder(TreeNode node)
+        {
+            if (node == null) return;
+            PrintInOrder(node.left);
+            Console.Write(node.val + " ");
+            PrintInOrder(node.right);
+        }
+    }
+}
---
+++ 108-ConvertSortedArrayBST/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _108_ConvertSortedArrayBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] case1 = { -10, -3, 0, 5, 9 };
+            ConvertArraySolution solution = new ConvertArraySolution();
+            TreeNode result1 = solution.SortedArrayToBST(case1);
+            solution.PrintInOrder(result1);
+
+
+            Console.WriteLine("====================================");
+
+            int[] case2 = { 1, 3 };
+            TreeNode result2 = solution.SortedArrayToBST(case2);
+            solution.PrintInOrder(result2);
+        }
+    }
+}
