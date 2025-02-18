7c330f484e469529ed5bbf47679e7ee64d161733
ngandolh <nganttk002@gmail.com>
2025-02-18 14:08:31 +0700

    [Done]

---
+++ 701-InsertBinarySearchTree/InsertBinarySearchTreeSolution.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace _701_InsertBinarySearchTree
+{
+    //https://leetcode.com/problems/insert-into-a-binary-search-tree/
+    public class InsertBinarySearchTreeSolution
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+            if (val == root.val)
+            {
+                return root;
+            }
+
+            if (val < root.val)
+            {
+                root.left = InsertIntoBST(root.left, val);
+            }
+            else
+            {
+                root.right = InsertIntoBST(root.right, val);
+            }
+            return root;
+        }
+
+        public void PrintInOrder(TreeNode root)
+        {
+            if (root == null) return;
+
+            PrintInOrder(root.left);
+            Console.Write(root.val + " ");
+            PrintInOrder(root.right);
+        }
+
+        public TreeNode BuildBST(int?[] values)
+        {
+            if (values == null || values.Length == 0 || values[0] == null)
+                return null;
+
+            TreeNode root = new TreeNode(values[0].Value);
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            int i = 1;
+            while (i < values.Length)
+            {
+                TreeNode current = queue.Dequeue();
+
+                if (values[i] != null) 
+                {
+                    current.left = new TreeNode(values[i].Value);
+                    queue.Enqueue(current.left);
+                }
+                i++;
+
+                if (i < values.Length && values[i] != null) 
+                {
+                    current.right = new TreeNode(values[i].Value);
+                    queue.Enqueue(current.right);
+                }
+                i++;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ 701-InsertBinarySearchTree/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _701_InsertBinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertBinarySearchTreeSolution solution = new InsertBinarySearchTreeSolution();
+            int?[] case1 = { 4, 2, 7, 1, 3};
+            int val1 = 5;
+
+            TreeNode root = solution.BuildBST(case1); 
+            root = solution.InsertIntoBST(root, val1);
+
+            Console.WriteLine("Cây BST sau khi chèn giá trị " + val1 + ": ");
+            solution.PrintInOrder(root);
+
+            Console.WriteLine("==============================================================");
+
+            int?[] case2 = { 40, 20, 60, 10, 30, 50, 70 };
+            int val2 = 25;
+            TreeNode root2 = solution.BuildBST(case2);
+            root2 = solution.InsertIntoBST(root2, val2);
+            Console.WriteLine("Cây BST sau khi chèn giá trị " + val2 + ":");
+            solution.PrintInOrder(root2);
+
+            Console.WriteLine("==============================================================");
+
+            int?[] case3 = { 4, 2, 7, 1, 3, null, null, null, null, null, null };
+            int val3 = 5;
+            TreeNode root3 = solution.BuildBST(case3);
+            root3 = solution.InsertIntoBST(root3, val3);
+            Console.WriteLine("Cây BST sau khi chèn giá trị " + val3 + ":");
+            solution.PrintInOrder(root3);
+        }
+    }
+}
