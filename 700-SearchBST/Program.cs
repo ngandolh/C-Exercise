3e049a8cd11e458d3c3b930e1df73d54bc59214c
ngandolh <nganttk002@gmail.com>
2025-02-19 10:43:20 +0700

    [Done]

---
+++ 700-SearchBST/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _700_SearchBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchBSTSolution searchBSTSolution = new SearchBSTSolution();
+            int[] case1 = { 4, 2, 7, 1, 3 };
+            int val1 = 2;
+            TreeNode root = searchBSTSolution.BuildBST(case1, 0, case1.Length - 1);
+            TreeNode result = searchBSTSolution.SearchBST(root, val1);
+            searchBSTSolution.PrintTree(result);
+
+            Console.WriteLine("======================================");
+
+            int val2 = 5;
+            TreeNode result2 = searchBSTSolution.SearchBST(root, val2);
+            searchBSTSolution.PrintTree(result2);
+
+
+        }
+    }
+}
---
+++ 700-SearchBST/SearchBSTSolution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _700_SearchBST
+{
+    public class SearchBSTSolution
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null || root.val == val) return root;
+
+            if (val < root.val)
+            {
+                return SearchBST(root.left, val);
+            }
+
+            return SearchBST(root.right, val);
+        }
+
+        public TreeNode BuildBST(int[] nums, int left, int right)
+        {
+            TreeNode root = null;
+            foreach (int num in nums)
+            {
+                root = InsertIntoBST(root, num);
+            }
+            return root;
+        }
+
+        private TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            if (val < root.val)
+                root.left = InsertIntoBST(root.left, val);
+            else
+                root.right = InsertIntoBST(root.right, val);
+
+            return root;
+        }
+
+        public TreeNode PrintTree(TreeNode root)
+        {
+            if (root == null) return null;
+            Console.WriteLine(root.val);
+            PrintTree(root.left);
+            PrintTree(root.right);
+            return root;
+        }
+    }
+}
