e4016bbde2d0a4a166bd3941d02f80ab0487e543
ngandolh <nganttk002@gmail.com>
2025-02-19 14:01:15 +0700

    [Done]

---
+++ 96-UniqueBST/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _96_UniqueBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueBSTSolution uniqueBSTSolution = new UniqueBSTSolution();
+            int case1 = 3;
+            int result = uniqueBSTSolution.NumTrees(case1);
+            System.Console.WriteLine(result);
+
+            Console.WriteLine("=========================================");
+
+            int case2 = 1;
+            int result2 = uniqueBSTSolution.NumTrees(case2);
+            System.Console.WriteLine(result2);
+
+            Console.WriteLine("=========================================");
+
+            int case3 = 19;
+            int result3 = uniqueBSTSolution.NumTrees(case3);
+            System.Console.WriteLine(result3);
+
+        }
+    }
+}
---
+++ 96-UniqueBST/UniqueBSTSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _96_UniqueBST
+{
+    public class UniqueBSTSolution
+    {
+        public int NumTrees(int n)
+        {
+            int result = (int)(BinomialCoeff(2 * n, n)/ (n + 1));
+            return result;
+        }
+
+        //Formula for Binomial Coefficient
+        public long BinomialCoeff(int n, int k)
+        {
+            long res = 1;
+            for (int i = 1; i <= k; i++)
+            {
+                res = res * (n - i + 1)/i ;
+            }
+            return res;
+        }
+    }
+}
