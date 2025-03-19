9946356f571b971e488f4e8d9b1ac388820f0cd6
ngandolh <nganttk002@gmail.com>
2025-03-19 23:08:48 +0700

    [DONE]

---
+++ 374-GuessNumberHigherOrLower/GuessNumberSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _374_GuessNumberHigherOrLower
+{
+    public class GuessNumberSolution
+    {
+        //guess(int num) is a function provided by the API
+        public int GuessNumber(int n)
+        {
+            int left = 1, right = n;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                int result = guess(mid);
+
+                if (result == 0) return mid;
+                else if (result == -1) right = mid - 1;
+                else if (result == 1) left = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ 374-GuessNumberHigherOrLower/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _374_GuessNumberHigherOrLower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
