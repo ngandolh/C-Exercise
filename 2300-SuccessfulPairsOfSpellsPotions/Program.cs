c553e4449bd0247dfc54695a2841248b505459d3
ngandolh <nganttk002@gmail.com>
2025-03-20 22:10:18 +0700

    [Done]

---
+++ 2300-SuccessfulPairsOfSpellsPotions/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _2300_SuccessfulPairsOfSpellsPotions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuccessfulPairsSolution successfulPairsSolution = new SuccessfulPairsSolution();
+            int[] spells = new int[] { 5, 1, 3 };
+            int[] potions = new int[] { 1, 2, 3, 4, 5 };
+            long success = 7;
+            int[] result = successfulPairsSolution.SuccessfulPairs(spells, potions, success);
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Second test case");
+            int[] spells2 = new int[] { 3, 1, 2 };
+            int[] potions2 = new int[] { 8, 5, 8 };
+            long success2 = 16;
+            int[] result2 = successfulPairsSolution.SuccessfulPairs(spells2, potions2, success2);
+            foreach (int i in result2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Third test case");
+            int[] spells3 = new int[] { 20, 26, 38, 23, 23, 20, 14, 30 };
+            int[] potions3 = new int[] { 24, 1, 7, 26, 19, 17, 7 };
+            long success3 = 510;
+            int[] result3 = successfulPairsSolution.SuccessfulPairs(spells3, potions3, success3);
+            foreach (int i in result3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 2300-SuccessfulPairsOfSpellsPotions/SuccessfulPairsSolution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2300_SuccessfulPairsOfSpellsPotions
+{
+    internal class SuccessfulPairsSolution
+    {
+        public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {
+            Array.Sort(potions);
+            int[] result = new int[spells.Length];
+
+            for (int i = 0; i < spells.Length; i++)
+            {
+                long minPotion = (long)Math.Ceiling((double)success / spells[i]);
+                int left = 0, right = potions.Length - 1;
+
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (potions[mid] >= minPotion)
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+
+                result[i] = potions.Length - left;
+            }
+            return result;
+        }
+
+        public int[] SuccessfulPairs2(int[] spells, int[] potions, long success)
+        {
+            //Problem: Index out of array when: potions.Length < spells.Length
+            int count = 0;
+            List<int> result = new List<int>();
+            for (int i = 0; i < spells.Length; i++)
+            {
+                for (int j = 0; j < potions.Length; j++)
+                {
+                    long cal = (long)spells[i] * potions[j];
+                    Console.WriteLine("Calculate: " + spells[i] + ", " + potions[j] + ", " + cal + ", j: " + j);
+                    if (cal >= success)
+                    {
+                        count++;
+                    }
+                }
+                result.Add(count);
+                count = 0;
+            }
+            return result.ToArray();
+        }
+    }
+}
