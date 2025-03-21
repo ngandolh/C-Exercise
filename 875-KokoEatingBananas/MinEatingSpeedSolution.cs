58513644e998f9afe8712ebd743a4a5d7f5f0b83
ngandolh <nganttk002@gmail.com>
2025-03-21 22:41:51 +0700

    [Done]

---
+++ 875-KokoEatingBananas/MinEatingSpeedSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _875_KokoEatingBananas
+{
+    internal class MinEatingSpeedSolution
+    {
+        public int MinEatingSpeed(int[] piles, int h)
+        {
+            int max = piles.Max();
+            int low = 1;
+            int high = max;
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                int totalHouse = 0;
+                for (int i = 0; i <= piles.Length - 1; i++)
+                {
+                    int hours = (int)Math.Ceiling(piles[i] / (double)mid);
+                    totalHouse += hours;
+                }
+
+                //If want to fast -> increase the speed -> increase the low
+                if (totalHouse > h)
+                {
+                    low = mid + 1;
+                }
+                //If want to slow or equal -> decrease the speed -> decrease the high
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            return low;
+        }
+    }
+}
---
+++ 875-KokoEatingBananas/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _875_KokoEatingBananas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinEatingSpeedSolution minEatingSpeedSolution = new MinEatingSpeedSolution();
+            int[] piles = new int[] { 3, 6, 7, 11 };
+            int h = 8;
+            int result = minEatingSpeedSolution.MinEatingSpeed(piles, h);
+            System.Console.WriteLine(result);
+
+            Console.WriteLine("=====================================");
+
+            int[] piles2 = new int[] { 30, 11, 23, 4, 20 };
+            int h2 = 5;
+            int result2 = minEatingSpeedSolution.MinEatingSpeed(piles2, h2);
+            System.Console.WriteLine(result2);
+
+            Console.WriteLine("=====================================");
+
+            int[] piles3 = new int[] { 30, 11, 23, 4, 20 };
+            int h3 = 6;
+            int result3 = minEatingSpeedSolution.MinEatingSpeed(piles3, h3);
+            System.Console.WriteLine(result3);
+        }
+    }
+}
