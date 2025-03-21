6278b8ea8515d94db5fbe27744eec7eb4bbf1c54
ngandolh <nganttk002@gmail.com>
2025-03-21 11:50:27 +0700

    [Done]

---
+++ 162-FindPeakElement/FindPeakElementSolution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _162_FindPeakElement
+{
+    internal class FindPeakElementSolution
+    {
+        //Input: nums = [1, 2, 3, 1]
+        //Output: 2
+        //Explanation: 3 is a peak element and your function should return the index number 2.
+        public int FindPeakElement(int[] nums)
+        {
+            if(nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length > 0)
+            {
+                var num = nums.Max();
+                return Array.IndexOf(nums, num);
+
+            }
+            return -1;
+        }
+
+        public int FindPeakElement2(int[] nums)
+        {
+            int n = nums.Length;
+            int low = 0, high = nums.Length - 1;
+
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                Console.WriteLine($"mid: {mid} - nums[mid] = {nums[mid]} - nums[mid+1] = {nums[mid+1]}");
+                if (nums[mid] < nums[mid + 1])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            return low;
+        }
+    }
+}
---
+++ 162-FindPeakElement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace _162_FindPeakElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            FindPeakElementSolution findPeakElementSolution = new FindPeakElementSolution();
+            int[] nums = { 1, 2, 3, 1 };
+            stopwatch.Start();
+            int result = findPeakElementSolution.FindPeakElement2(nums);
+            stopwatch.Stop();
+            System.Console.WriteLine(result);
+            System.Console.WriteLine("Time elapsed: {0:0.000}ms", stopwatch.Elapsed.TotalMilliseconds);
+
+            stopwatch.Restart();
+            Console.WriteLine("==========================================================");
+            int[] nums2 = { 1, 2, 1, 3, 5, 6, 4 };
+            int result2 = findPeakElementSolution.FindPeakElement2(nums2);
+            stopwatch.Stop();
+            System.Console.WriteLine(result2);
+            System.Console.WriteLine("Time elapsed: {0:0.000}ms", stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
