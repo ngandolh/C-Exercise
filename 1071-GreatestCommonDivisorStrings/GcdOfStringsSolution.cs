f4548f57f0547a758469bb03639d1341c815c4e9
ngandolh <nganttk002@gmail.com>
2025-03-22 21:26:18 +0700

    [Done]

---
+++ 1071-GreatestCommonDivisorStrings/GcdOfStringsSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1071_GreatestCommonDivisorStrings
+{
+    internal class GcdOfStringsSolution
+    {
+        public string GcdOfStrings2(string str1, string str2)
+        {
+            int i = 0, j = 0;
+            while (i < str1.Length && j < str2.Length)
+            {
+                if (str1[i] != str2[j])
+                {
+                    return "";
+                }
+                i++;
+                j++;
+            }
+            return i == str1.Length ? str1 : str2;
+        }
+
+        public string GcdOfStrings(string str1, string str2)
+        {
+            if ((str1 + str2) != (str2 + str1)) return "";
+            return str1.Substring(0, GCD(str1.Length, str2.Length));
+        }
+
+        private int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
---
+++ 1071-GreatestCommonDivisorStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1071_GreatestCommonDivisorStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GcdOfStringsSolution gcdOfStringsSolution = new GcdOfStringsSolution();
+            string str1 = "ABCABC";
+            string str2 = "ABC";
+            string result = gcdOfStringsSolution.GcdOfStrings(str1, str2);
+            System.Console.WriteLine(result);
+
+            str1 = "ABABAB";
+            str2 = "ABAB";
+            result = gcdOfStringsSolution.GcdOfStrings(str1, str2);
+            System.Console.WriteLine(result);
+
+            str1 = "LEET";
+            str2 = "CODE";
+            result = gcdOfStringsSolution.GcdOfStrings(str1, str2);
+            System.Console.WriteLine(result);
+
+            str1 = "ABCDEF";
+            str2 = "AFEDSF";
+            result = gcdOfStringsSolution.GcdOfStrings(str1, str2);
+            System.Console.WriteLine(result);
+
+        }
+    }
+}
