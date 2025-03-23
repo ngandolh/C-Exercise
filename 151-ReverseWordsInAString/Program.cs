b43daeec87040c0f996ba132addbc91730a6fbe2
ngandolh <nganttk002@gmail.com>
2025-03-23 14:59:56 +0700

    [Done]

---
+++ 151-ReverseWordsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace _151_ReverseWordsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            ReverseWordsSolution reverseWordsSolution = new ReverseWordsSolution();
+            string s = "the sky is blue";
+            stopwatch.Start();
+            string result = reverseWordsSolution.ReverseWords(s);
+            stopwatch.Stop();
+            Console.WriteLine(result);
+            Console.WriteLine("Timeline: " + stopwatch.ElapsedTicks);
+
+            Console.WriteLine("=================================================");
+            //string s2 "  hello world  ";
+            //string result2 = reverseWordsSolution.ReverseWords(s2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine("=================================================");
+
+            string s3 = "a good   example";
+            stopwatch.Restart();
+            string result3 = reverseWordsSolution.ReverseWords(s3);
+            stopwatch.Stop();
+            Console.WriteLine(result3);
+            Console.WriteLine("Timeline: " + stopwatch.ElapsedTicks);
+            Console.WriteLine("=================================================");
+        }
+    }
+}
---
+++ 151-ReverseWordsInAString/ReverseWordsSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _151_ReverseWordsInAString
+{
+    internal class ReverseWordsSolution
+    {
+        public string ReverseWords(string s)
+        {
+            string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words); 
+            return string.Join(" ", words);
+        }
+    }
+}
