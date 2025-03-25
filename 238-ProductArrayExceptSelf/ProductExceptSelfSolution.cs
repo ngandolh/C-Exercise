756a91c3e91670b84228e109a96bdfea7cbfa9e7
ngandolh <nganttk002@gmail.com>
2025-03-25 15:08:10 +0700

    [Done]

---
+++ 238-ProductArrayExceptSelf/ProductExceptSelfSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _238_ProductArrayExceptSelf
+{
+    internal class ProductExceptSelfSolution
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            
+            if (nums == null || nums.Length == 0)
+            {
+                return new int[0];
+            }
+
+            int n = nums.Length;
+            int[] result = new int[n];
+            int[] left = new int[n];
+            left[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                left[i] = left[i- 1] * nums[i - 1];
+            }
+
+            int right = 1;
+            for(int i = n - 1; i >= 0; i--)
+            {
+                result[i] = left[i] * right;
+                right *= nums[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 238-ProductArrayExceptSelf/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _238_ProductArrayExceptSelf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductExceptSelfSolution productExceptSelfSolution = new ProductExceptSelfSolution();
+            int[] nums = new int[] { 1, 2, 3, 4 };
+            int[] result = productExceptSelfSolution.ProductExceptSelf(nums);
+            foreach (int i in result)
+            {
+                System.Console.WriteLine(i);
+            }
+
+            Console.WriteLine("================================================================");
+        }
+    }
+}
