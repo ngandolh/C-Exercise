449e513e66ed50dc89b5afef9a37f688008d9807
ngandolh <nganttk002@gmail.com>
2025-03-25 21:24:48 +0700

    [Done]

---
+++ 334-IncreasingTripletSubsequence/IncreasingTripletSolution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _334_IncreasingTripletSubsequence
+{
+    internal class IncreasingTripletSolution
+    {
+        public bool IncreasingTriplet2(int[] nums)
+        {
+            int n = nums.Length;
+            bool result = false;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i == n) break; 
+                if(nums[i] < nums[i + 1] && nums[i + 1] < nums[i + 2])
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        public bool IncreasingTriplet3(int[] nums)
+        {
+            int n = nums.Length;
+            bool result = false;
+
+            bool first = false;
+            for(int i = 0; i < n; i++)
+            {
+                int firstElement = nums[i];
+                first = nums.Contains(firstElement + 1) && nums.Contains(firstElement + 2);
+                Console.WriteLine($"i = {i} 1E = {firstElement} 2E = {firstElement + 1} 3E = {firstElement + 2} first = {first}");
+                if (first)
+                {
+                    break;
+                }
+            }
+
+            if (!first)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int first = int.MaxValue, second = int.MaxValue;
+            Console.WriteLine($"first = {first} second = {second}");
+            foreach (int num in nums)
+            {
+                if (num <= first)
+                {
+                    first = num; 
+                    Console.WriteLine($"first = {first}");
+                }
+                else if (num <= second)
+                {
+                    second = num; 
+                    Console.WriteLine($"second = {second}");
+                }
+                //third > second > first
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 334-IncreasingTripletSubsequence/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _334_IncreasingTripletSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IncreasingTripletSolution increasingTripletSolution = new IncreasingTripletSolution();
+            //int[] nums1 = { 1, 2, 3, 4, 5 };
+            //Console.WriteLine("Case 1");
+            //bool result1 = increasingTripletSolution.IncreasingTriplet(nums1);
+            //System.Console.WriteLine(result1); // True
+
+            //Console.WriteLine("=====================================");
+
+            //int[] nums2 = { 5, 4, 3, 2, 1 };
+            //Console.WriteLine("Case 2");
+            //bool result2 = increasingTripletSolution.IncreasingTriplet(nums2);
+            //System.Console.WriteLine(result2); // False
+
+            //Console.WriteLine("=====================================");
+
+            Console.WriteLine("Case 3");
+            int[] nums3 = { 20, 100, 10, 12, 5, 13 };
+            bool result3 = increasingTripletSolution.IncreasingTriplet(nums3);
+            System.Console.WriteLine(result3); // True
+        }
+    }
+}
