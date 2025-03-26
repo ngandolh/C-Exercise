7932b8507f9d6aa8108877ce7ea6988fc6f859b6
ngandolh <nganttk002@gmail.com>
2025-03-26 21:41:44 +0700

    [Done]

---
+++ 283-MoveZeroes/MoveZeroesSolution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _283_MoveZeroes
+{
+    internal class MoveZeroesSolution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            if(nums.Length == 1) { return; }
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
+                    {
+                        if (nums[j] != 0)
+                        {
+                            nums[i] = nums[j];
+                            nums[j] = 0;
+                            Console.WriteLine(string.Join(", ", nums));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void MoveZeroes2(int[] nums)
+        {
+            int n = nums.Length;
+            int p = 0, i = 0;
+
+            while (i < n)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[i - p] = nums[i];
+                    Console.WriteLine(string.Join(", ", nums));
+                }
+                else
+                {
+                    p++;
+                }
+                i++;
+            }
+
+            i = n - p;
+            while(i < n)
+            {
+                nums[i++] = 0;
+            }
+        }
+    }
+}
---
+++ 283-MoveZeroes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _283_MoveZeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveZeroesSolution moveZeroesSolution = new MoveZeroesSolution();
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+            Console.WriteLine("Before: " + string.Join(", ", nums));
+            moveZeroesSolution.MoveZeroes2(nums);
+            Console.WriteLine("After: " + string.Join(", ", nums));
+        }
+    }
+}
