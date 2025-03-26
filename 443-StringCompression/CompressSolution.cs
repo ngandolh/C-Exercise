d39f6a13638ea48432387b1813505d38442b55a4
ngandolh <nganttk002@gmail.com>
2025-03-26 14:31:21 +0700

    [Done]

---
+++ 443-StringCompression/CompressSolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _443_StringCompression
+{
+    internal class CompressSolution
+    {
+        public int Compress(char[] chars)
+        {
+            if (chars.Length == 1)
+            {
+                return 1;
+            }
+            int writeIndex = 0;
+            int i = 0;
+
+            while (i < chars.Length)
+            {
+                int count = 0;
+                char currentChar = chars[i];
+
+                while (i < chars.Length && chars[i] == currentChar)
+                {
+                    i++;
+                    count++;
+                }
+
+                chars[writeIndex++] = currentChar;
+
+                if (count > 1)
+                {
+                    foreach (char c in count.ToString())
+                    {
+                        chars[writeIndex++] = c;
+                    }
+                }
+            }
+
+            return writeIndex;
+        }
+    }
+}
---
+++ 443-StringCompression/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _443_StringCompression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompressSolution compressSolution = new CompressSolution();
+            char[] chars1 = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }; // 6
+            var result = compressSolution.Compress(chars1);
+            Console.WriteLine(result);
+
+            Console.WriteLine("==========");
+
+            char[] chars2 = new char[] { 'a' }; //1
+            var result2 = compressSolution.Compress(chars2);
+            Console.WriteLine(result2);
+
+            Console.WriteLine("==========");
+
+            char[] chars3 = new char[] { 'a', 'b', 'c' };
+            var result3 = compressSolution.Compress(chars3);
+            Console.WriteLine(result3);
+
+            Console.WriteLine("==========");
+
+            char[] chars4 = new char[] { 'a', 'a', 'a', 'a', 'a', 'b' };
+            var result4 = compressSolution.Compress(chars4);
+            Console.WriteLine(result4);
+            Console.WriteLine("==========");
+
+        }
+    }
+}
