96bdcc966422e938389c94469867581721f38091
ngandolh <nganttk002@gmail.com>
2025-03-27 23:58:40 +0700

    [Done]

---
+++ 392-IsSubsequence/IsSubsequenceSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _392_IsSubsequence
+{
+    internal class IsSubsequenceSolution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int sIndex = 0, tIndex = 0;
+            while (sIndex < s.Length && tIndex < t.Length)
+            {
+                Console.WriteLine("sIndex: " + sIndex + " tIndex: " + tIndex);
+                if (s[sIndex] == t[tIndex])
+                {
+                    sIndex++;
+                }
+                tIndex++;
+            }
+
+            return sIndex == s.Length;
+        }
+    }
+}
---
+++ 392-IsSubsequence/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _392_IsSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsSubsequenceSolution isSubsequenceSolution = new IsSubsequenceSolution();
+            string s = "abc";
+            string t = "ahbgdc";
+            bool result = isSubsequenceSolution.IsSubsequence(s, t);
+            Console.WriteLine("IsSubsequence: " + result);
+
+            Console.WriteLine("**********************");
+
+            string s2 = "axc";
+            string t2 = "ahbgdc";
+            bool result2 = isSubsequenceSolution.IsSubsequence(s2, t2);
+            Console.WriteLine("IsSubsequence: " + result2);
+        }
+    }
+}
