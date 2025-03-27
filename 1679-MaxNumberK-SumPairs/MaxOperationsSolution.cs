287c47f3640d0e13bf1f92f6bbfaa62b9131934a
ngandolh <nganttk002@gmail.com>
2025-03-27 13:15:31 +0700

    [Done]

---
+++ 1679-MaxNumberK-SumPairs/MaxOperationsSolution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1679_MaxNumberK_SumPairs
+{
+    internal class MaxOperationsSolution
+    {
+        public int MaxOperations(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            int count = 0;
+            int left = 0, run = nums.Length - 1;
+
+            while (left < run)
+            {
+                int sum = nums[left] + nums[run];
+                Console.WriteLine($"nums[{left}]={nums[left]} + nums[{run}]={nums[run]} = {k}");
+                if (sum == k)
+                {
+                    count++;
+                    left++;
+                    run--;
+                }
+                else if (sum < k) 
+                {
+                    left++;
+                }
+                else
+                {
+                    run--;
+                }
+            }
+            return count;
+        }
+
+        //        for (int i = 0; i<nums.Length - 1; i++)
+        //            {
+        //                for (int j = 0; j<nums.Length; j++)
+        //                {
+        //                    if (nums[i] + nums[j] == k)
+        //                    {
+        //                        Console.WriteLine($"nums[{i}]={nums[i]} + nums[{j}]={nums[j]} = {k}");
+        //                        count++;
+        //                    }
+        //}
+        //            }
+    }
+}
---
+++ 1679-MaxNumberK-SumPairs/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1679_MaxNumberK_SumPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxOperationsSolution maxOperationsSolution = new MaxOperationsSolution();
+            //int[] nums = new int[] { 1, 2, 3, 4 }; // Output: 2
+            //int k = 5;
+            //int result = maxOperationsSolution.MaxOperations(nums, k);
+            //Console.WriteLine(result);
+
+            //Console.WriteLine("======================================");
+            //int[] nums2 = new int[] { 3, 1, 3, 4, 3 }; // Output: 1
+            //int k2 = 6;
+            //int result2 = maxOperationsSolution.MaxOperations(nums2, k2);
+            //Console.WriteLine(result2);
+
+            Console.WriteLine("======================================");
+            int[] nums3 = new int[] { 4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4 }; // Output: 2
+            int k3 = 2;
+            int result3 = maxOperationsSolution.MaxOperations(nums3, k3);
+            Console.WriteLine(result3);
+        }
+    }
+}
