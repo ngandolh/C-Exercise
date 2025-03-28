65d0441528da91eee66ae3c4ec5420213706e378
ngandolh <nganttk002@gmail.com>
2025-03-28 11:50:20 +0700

    [Done]

---
+++ 450-DeleteNodeBST/DeleteNodeSolution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _450_DeleteNodeBST
+{
+    internal class DeleteNodeSolution
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            //Find the node = key
+            if (root == null) return null;
+
+            //Case 1: key is leaf nodea
+            //Case 2: key is single child node
+            //Case 3: key is two children node
+
+            if (key < root.val)
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else if (key > root.val)
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            else
+            {
+                if (root.left == null) return root.right;
+                if (root.right == null) return root.left;
+
+                //Case 3: key is two children node
+                TreeNode minNode = FindMin(root.right); //4
+                Console.WriteLine("Min Node: " + minNode.val);
+                root.val = minNode.val; //change value = 3 to 4 
+                //TreeNode: 5, 4, 6, 2, 4, 7
+                root.right = DeleteNode(root.right, minNode.val); //Delete 4
+            }
+
+            return root;
+        }
+
+        public TreeNode FindMin(TreeNode node)
+        {
+            while (node.left != null) node = node.left;
+            return node;
+        }
+
+        public void PrintTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(root.val + " ");
+            PrintTree(root.left);
+            PrintTree(root.right);
+        }
+    }
+}
---
+++ 450-DeleteNodeBST/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _450_DeleteNodeBST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteNodeSolution deleteNodeSolution = new DeleteNodeSolution();
+            TreeNode root = new TreeNode(5);
+            root.left = new TreeNode(3);
+            root.right = new TreeNode(6);
+            root.left.left = new TreeNode(2);
+            root.left.right = new TreeNode(4);
+            root.right.right = new TreeNode(7);
+
+            Console.WriteLine("BST before deleting node:");
+            deleteNodeSolution.PrintTree(root);
+            Console.WriteLine("\n=====================================");
+
+            TreeNode result = deleteNodeSolution.DeleteNode(root, 0);
+
+            Console.WriteLine("BST after deleting node:");
+            deleteNodeSolution.PrintTree(result);
+            Console.WriteLine("\n=====================================");
+        }
+    }
+}
