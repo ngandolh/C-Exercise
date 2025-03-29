bad3f77081bd5c6a2a77e914a604e05f04385e1b
ngandolh <nganttk002@gmail.com>
2025-03-29 12:49:52 +0700

    [Done]

---
+++ 206-ReverseLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _206_ReverseLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseListSolution reverseListSolution = new ReverseListSolution();
+        }
+    }
+}
---
+++ 206-ReverseLinkedList/ReverseListSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _206_ReverseLinkedList
+{
+    internal class ReverseListSolution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode pre = null;
+            ListNode current = head;
+            while (current != null)
+            {
+                ListNode next = current.next;
+                current.next = pre;
+                pre = current;
+                current = next;
+            }
+
+            return pre;
+        }
+
+        //public ListNode ReverseList(ListNode head)
+        //{
+        //    ListNode prev = null;
+        //    ListNode current = head;
+
+        //    while (current != null)
+        //    {
+        //        ListNode next = current.next; // Lưu lại node tiếp theo
+        //        current.next = prev; // Đảo chiều liên kết
+        //        prev = current; // Di chuyển prev lên current
+        //        current = next; // Di chuyển current lên next
+        //    }
+        //    return prev; // prev sẽ là node đầu tiên sau khi đảo ngược
+        //}
+    }
+}
