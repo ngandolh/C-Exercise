23f04b46d447b4c4565b42935de85caca7bde75a
ngandolh <nganttk002@gmail.com>
2025-03-30 23:43:03 +0700

    [Done]

---
+++ 2095-DeleteMiddleNodeLinkedList/DeleteMiddleSolution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2095_DeleteMiddleNodeLinkedList
+{
+    internal class DeleteMiddleSolution
+    {
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            //Case: ListNode still 1 node
+            if (head == null || head.next == null) return null;
+
+            int n = CountNodes(head);
+            int middle = n /2;
+         
+            ListNode current = head;
+            ListNode prev = null;
+
+            int count = 0;
+            Console.WriteLine("Middle: " + middle);
+
+            if (middle == 0)
+            {
+                return head.next;
+            }
+            
+            while(count < middle)
+            {
+                prev = current;
+                Console.WriteLine("Prev: ");
+                PrintNodes(prev);
+                current = current.next;
+                Console.WriteLine("Current: ");
+                PrintNodes(current);
+                count++;
+            }
+
+            prev.next = current.next;
+
+            return head;
+        }
+
+        public int CountNodes(ListNode head)
+        {
+            int count = 0;
+            while (head != null)
+            {
+                count++;
+                head = head.next;
+            }
+            return count;
+        }
+
+        public void PrintNodes(ListNode head)
+        {
+            while(head != null)
+            {
+                Console.Write(head.val + " -> ");
+                head = head.next;
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ 2095-DeleteMiddleNodeLinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2095_DeleteMiddleNodeLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteMiddleSolution deleteMiddleSolution = new DeleteMiddleSolution();
+            //ListNode head = new ListNode(1, 
+            //                new ListNode(3, 
+            //                new ListNode(4, 
+            //                new ListNode(7, 
+            //                new ListNode(1, 
+            //                new ListNode(2, 
+            //                new ListNode(6)))))));
+            //ListNode result = deleteMiddleSolution.DeleteMiddle(head);
+            //deleteMiddleSolution.PrintNodes(result);
+
+            ListNode head2 = new ListNode(1,
+                            new ListNode(2,
+                            new ListNode(3,
+                            new ListNode(4))));
+            ListNode result2 = deleteMiddleSolution.DeleteMiddle(head2);
+            deleteMiddleSolution.PrintNodes(result2);
+        }
+    }
+}
