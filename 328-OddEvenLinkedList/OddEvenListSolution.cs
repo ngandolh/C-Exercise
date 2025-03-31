f728cc1e8f1c137db1fc49a0ca1d2d2bf139ce77
ngandolh <nganttk002@gmail.com>
2025-03-31 22:32:35 +0700

    [Done]

---
+++ 328-OddEvenLinkedList/OddEvenListSolution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _328_OddEvenLinkedList
+{
+    internal class OddEvenListSolution
+    {
+        public ListNode OddEvenList(ListNode head)
+        {
+            //Odd - Even number of nodes
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = even;
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                Console.WriteLine("Odd.next = even.next: ");
+                PrintList(odd);
+
+                odd = odd.next;
+                Console.WriteLine("Odd = odd.next: ");
+                PrintList(odd);
+
+                even.next = odd.next;
+                Console.WriteLine("Even.next = odd.next: ");
+                PrintList(even);
+
+                even = even.next;
+                Console.WriteLine("Even = event.next: ");
+                PrintList(even);
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Even head: ");
+            PrintList(evenHead);
+
+            odd.next = evenHead; //connect odd to even head
+
+            return head;
+        }
+
+        public void PrintList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.val + " -> ");
+                head = head.next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 328-OddEvenLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _328_OddEvenLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OddEvenListSolution oddEvenListSolution = new OddEvenListSolution();
+            ListNode head = new ListNode(2);
+            head.next = new ListNode(1);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(5);
+            head.next.next.next.next = new ListNode(6);
+            head.next.next.next.next.next = new ListNode(4);
+            head.next.next.next.next.next.next = new ListNode(7);
+
+            ListNode result = oddEvenListSolution.OddEvenList(head);
+            oddEvenListSolution.PrintList(result);
+
+        }
+    }
+}
