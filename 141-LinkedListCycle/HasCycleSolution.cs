dce961734694edf26b5d90deede7feb7708f8bba
ngandolh <nganttk002@gmail.com>
2025-03-31 13:49:11 +0700

    [Done]

---
+++ 141-LinkedListCycle/HasCycleSolution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _141_LinkedListCycle
+{
+    internal class HasCycleSolution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            //Cycle detection using Floyd's Tortoise and Hare algorithm
+            if (head == null || head.next == null)
+            {
+                return false;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+            //Pos = -1 means no cycle
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+            //Pos != -1 means there is a cycle
+        }
+
+        public int CountNodes(ListNode head)
+        {
+            int count = 0;
+            while (head != null)
+            {
+                count++;
+                head = head.next;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 141-LinkedListCycle/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _141_LinkedListCycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HasCycleSolution hasCycleSolution = new HasCycleSolution();
+            ListNode head = new ListNode(3);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(0);
+            head.next.next.next = new ListNode(-4);
+            head.next.next.next.next = head.next;
+
+            bool hasCycle = hasCycleSolution.HasCycle(head); //Expected output: true
+            Console.WriteLine("Has cycle: " + hasCycle);
+
+            ListNode head2 = new ListNode(1);
+            head2.next = new ListNode(2);
+
+            bool result2 = hasCycleSolution.HasCycle(head2); //Expected output: false
+            Console.WriteLine("Has cycle: " + result2);
+        }
+    }
+}
