c6adf13fdecb5a577c0a09090731c98bd4be501f
ngandolh <nganttk002@gmail.com>
2025-04-01 14:05:00 +0700

    [Done]

---
+++ 2130-MaximumTwinSumLinkedList/PairSumSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2130_MaximumTwinSumLinkedList
+{
+    internal class PairSumSolution
+    {
+        public int PairSum(ListNode head)
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+
+            //Change ListNode to array
+            List<int> values = new List<int>();
+            ListNode temp = head;
+            while (temp != null)
+            {
+                values.Add(temp.val);
+                temp = temp.next;
+            }
+
+            //n is even
+            int n = CountNodes(head);
+            int twinNumber = (n / 2) - 1;
+            int result = 0;
+            for(int i = 0; i < n/2; i++)
+            {
+                int totalTwin = values[i] + values[n - 1 - i];
+                result = Math.Max(result, totalTwin);
+            }
+
+            return result;
+
+        }
+
+        public int CountNodes(ListNode head)
+        {
+            int count = 0;
+            while (head != null)
+            {
+                count++;
+                head = head.next;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 2130-MaximumTwinSumLinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2130_MaximumTwinSumLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PairSumSolution pairSumSolution = new PairSumSolution();
+            ListNode head = new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(3))));
+            int result = pairSumSolution.PairSum(head);
+            System.Console.WriteLine("Case 1: " + result);
+
+            ListNode head2 = new ListNode(1, new ListNode(100000));
+            int result2 = pairSumSolution.PairSum(head2);
+            System.Console.WriteLine("Case 2: " + result2);
+        }
+    }
+}
