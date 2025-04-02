9e181128b2122c8cae1941b0c62964532f293f48
ngandolh <nganttk002@gmail.com>
2025-04-02 22:54:43 +0700

    [Done]

---
+++ 724-FindPivotIndex/PivotIndexSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _724_FindPivotIndex
+{
+    internal class PivotIndexSolution
+    {
+        public int PivotIndex(int[] nums)
+        {
+            //Total sum of the array
+            int totalSum = nums.Sum();
+            int leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (leftSum == totalSum - leftSum - nums[i])
+                {
+                    return i;
+                }
+                else
+                {
+                    leftSum += nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 724-FindPivotIndex/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _724_FindPivotIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PivotIndexSolution pivotIndexSolution = new PivotIndexSolution();
+            int[] nums = new int[] { 1, 7, 3, 6, 5, 6 };
+            int result = pivotIndexSolution.PivotIndex(nums);
+            Console.WriteLine(result);
+
+            Console.WriteLine("=====================================");
+
+            int[] nums2 = new int[] { 1, 2, 3 };
+            int result2 = pivotIndexSolution.PivotIndex(nums2);
+            Console.WriteLine(result2);
+
+            Console.WriteLine("=====================================");
+
+            int[] nums3 = new int[] { -1, -1, -1, -1, -1, 0 };
+            int result3 = pivotIndexSolution.PivotIndex(nums3);
+            Console.WriteLine(result3);
+        }
+    }
+}
