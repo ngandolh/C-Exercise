83997e037294db303631bee51f55dc9ca9d780bb
ngandolh <nganttk002@gmail.com>
2025-04-02 20:27:07 +0700

    [Done]

---
+++ 1732-FindTheHighestAltitude/LargestAltitudeSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1732_FindTheHighestAltitude
+{
+    internal class LargestAltitudeSolution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int result = int.MinValue;
+            int total = 0;
+
+            for (int i = 0; i < gain.Length; i++)
+            {
+                total += gain[i];
+                Console.WriteLine("Gain[i]: " + gain[i] + " = " + total + " (total)");
+              
+                if(result < total)
+                {
+                    result = total;
+                }
+
+            }
+
+            if (result < 0)
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 1732-FindTheHighestAltitude/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _1732_FindTheHighestAltitude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LargestAltitudeSolution largestAltitudeSolution = new LargestAltitudeSolution();
+            //int[] gain = new int[] { -5, 1, 5, 0, -7 }; //Expected output: 1
+            //int result = largestAltitudeSolution.LargestAltitude(gain);
+            //Console.WriteLine(
+            //    $"The highest altitude is {result}");
+
+            //Console.WriteLine("========================================");
+
+            //int[] gain2 = new int[] { -4, -3, -2, -1, 4, 3, 2 }; //Expected output: 0
+            //int result2 = largestAltitudeSolution.LargestAltitude(gain2);
+            //Console.WriteLine(
+            //    $"The highest altitude is {result2}");
+
+            int[] gain3 = new int[] { 44, 32, -9, 52, 23, -50, 50, 33, -84, 47, -14, 84, 36, 
+                -62, 37, 81, -36, -85, -39, 67, -63, 64, -47, 95, 91, -40, 65, 67, 92, -28, 97, 100, 81 };
+            int result3 = largestAltitudeSolution.LargestAltitude(gain3);
+            Console.WriteLine(
+                $"The highest altitude is {result3}");
+        }
+    }
+}
