4b10995dc9e1b0a005ca94f1b74b1d91c0c1ac74
ngandolh <nganttk002@gmail.com>
2025-04-04 23:07:02 +0700

    [Done]

---
+++ 1657-DetermineTwoStringsAreClose/CloseStringsSolution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1657_DetermineTwoStringsAreClose
+{
+    internal class CloseStringsSolution
+    {
+        public bool CloseStrings(string word1, string word2)
+        {
+            if (word1.Length != word2.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> countWord1 = new Dictionary<char, int>();
+            Dictionary<char, int> countWord2 = new Dictionary<char, int>();
+
+            foreach (char c in word1)
+            {
+                if (countWord1.ContainsKey(c))
+                {
+                    countWord1[c]++;
+                }
+                else
+                {
+                    countWord1[c] = 1;
+                }
+            }
+
+            foreach (char c in word2)
+            {
+                if (countWord2.ContainsKey(c))
+                {
+                    countWord2[c]++;
+                }
+                else
+                {
+                    countWord2[c] = 1;
+                }
+            }
+
+            if (!countWord1.Keys.OrderBy(c => c).SequenceEqual(countWord2.Keys.OrderBy(c => c)))
+            {
+                return false;
+            }
+
+            if (!countWord2.Values.OrderBy(v => v).SequenceEqual(countWord2.Values.OrderBy(v => v)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ 1657-DetermineTwoStringsAreClose/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1657_DetermineTwoStringsAreClose
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CloseStringsSolution closeStringsSolution = new CloseStringsSolution();
+            //string word1 = "abc";
+            //string word2 = "bca";
+            //bool result = closeStringsSolution.CloseStrings(word1, word2);
+            //Console.WriteLine($"Are the strings '{word1}' and '{word2}' close? {result}"); //Expected output: true
+
+            //string word3 = "cabbba";
+            //string word4 = "abbccc";
+            //bool result2 = closeStringsSolution.CloseStrings(word3, word4);
+            //Console.WriteLine($"Are the strings '{word3}' and '{word4}' close? {result2}"); //Expected output: true
+
+            string word5 = "uau";
+            string word6 = "ssx";
+            bool result3 = closeStringsSolution.CloseStrings(word5, word6);
+            Console.WriteLine($"Are the strings '{word5}' and '{word6}' close? {result3}"); //Expected output: false
+
+
+        }
+    }
+}
