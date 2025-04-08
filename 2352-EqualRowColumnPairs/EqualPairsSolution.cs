6361ef2d74073c2a97215203a009432788dd88ba
ngandolh <nganttk002@gmail.com>
2025-04-08 22:19:40 +0700

    [Progress]

---
+++ 2352-EqualRowColumnPairs/EqualPairsSolution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2352_EqualRowColumnPairs
+{
+    internal class EqualPairsSolution
+    {
+        public int EqualPairs(int[][] grid)
+        {
+            Dictionary<string, int> rowCount = new Dictionary<string, int>();
+            Dictionary<string, int> columnCount = new Dictionary<string, int>();
+            int n = grid.Length;
+            int m = grid[0].Length;
+            int result = 0;
+
+            //Adding rows to the dictionary 
+            foreach (var row in grid)
+            {
+                string rowKey = string.Join(",", row);
+                if (rowCount.ContainsKey(rowKey))
+                {
+                    rowCount[rowKey]++;
+                }
+                else
+                {
+                    rowCount[rowKey] = 1;
+                }
+                Console.WriteLine(rowKey);
+            }
+
+            //Adding columns to the dictionary
+            for (int j = 0; j < m; j++)
+            {
+                List<int> column = new List<int>();
+                for (int k = 0; k < m; k++)
+                {
+                    column.Add(grid[m][j]);
+                }
+                string columnKey = string.Join(",", column);
+                if (columnCount.ContainsKey(columnKey))
+                {
+                    columnCount[columnKey]++;
+                }
+                else
+                {
+                    columnCount[columnKey] = 1;
+                }
+            }
+
+            //Comparing rows and columns
+            foreach (var row in rowCount)
+            {
+                if (columnCount.ContainsKey(row.Key))
+                {
+                    result += row.Value * columnCount[row.Key];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 2352-EqualRowColumnPairs/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _2352_EqualRowColumnPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualPairsSolution equalPairsSolution = new EqualPairsSolution();
+            int[][] grid = new int[][]
+            {
+                new int[] { 3, 2, 1 },
+                new int[] { 1, 7, 6 },
+                new int[] { 2, 7, 7 }
+            };
+            int result = equalPairsSolution.EqualPairs(grid);
+            Console.WriteLine(result); // Expected output: 2
+        }
+    }
+}
