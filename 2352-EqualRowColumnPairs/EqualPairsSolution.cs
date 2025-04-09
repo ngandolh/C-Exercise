b61885a8b5b82c4999a11dacb766d40518f46d1d
ngandolh <nganttk002@gmail.com>
2025-04-09 10:51:34 +0700

    [Done]

--- 2352-EqualRowColumnPairs/EqualPairsSolution.cs
+++ 2352-EqualRowColumnPairs/EqualPairsSolution.cs
@@ -8,18 +8,19 @@ namespace _2352_EqualRowColumnPairs
 {
     internal class EqualPairsSolution
     {
+        //Time Complexity: O(n2)
         public int EqualPairs(int[][] grid)
         {
             Dictionary<string, int> rowCount = new Dictionary<string, int>();
             Dictionary<string, int> columnCount = new Dictionary<string, int>();
             int n = grid.Length;
-            int m = grid[0].Length;
             int result = 0;
 
-            //Adding rows to the dictionary 
+            //Adding rows to the dictionary O(n2)
             foreach (var row in grid)
             {
-                string rowKey = string.Join(",", row);
+                string rowKey = string.Join(",", row); //O(n)
+
                 if (rowCount.ContainsKey(rowKey))
                 {
                     rowCount[rowKey]++;
@@ -28,16 +29,21 @@ namespace _2352_EqualRowColumnPairs
                 {
                     rowCount[rowKey] = 1;
                 }
-                Console.WriteLine(rowKey);
             }
 
-            //Adding columns to the dictionary
-            for (int j = 0; j < m; j++)
+            //Print rowCount
+            foreach(var row in rowCount.Keys)
+            {
+                Console.WriteLine("rowKey: " + row);
+            }
+
+            //Adding columns to the dictionary O(n2)
+            for (int j = 0; j < n; j++)
             {
                 List<int> column = new List<int>();
-                for (int k = 0; k < m; k++)
+                for (int k = 0; k < n; k++)
                 {
-                    column.Add(grid[m][j]);
+                    column.Add(grid[k][j]);
                 }
                 string columnKey = string.Join(",", column);
                 if (columnCount.ContainsKey(columnKey))
@@ -50,12 +56,20 @@ namespace _2352_EqualRowColumnPairs
                 }
             }
 
+            //Print columnCount
+            foreach (var column in columnCount)
+            {
+                Console.WriteLine("columnCount.Key - column.Value: " + 
+                    column.Key + " - " + column.Value);
+            }
+
             //Comparing rows and columns
             foreach (var row in rowCount)
             {
-                if (columnCount.ContainsKey(row.Key))
+                Console.WriteLine("columnCount.Key - row.Key: " + row.Key);
+                if (columnCount.ContainsKey(row.Key))                       //O(n)
                 {
-                    result += row.Value * columnCount[row.Key];
+                    result += row.Value * columnCount[row.Key]; 
                 }
             }
 
--- 2352-EqualRowColumnPairs/Program.cs
+++ 2352-EqualRowColumnPairs/Program.cs
@@ -5,14 +5,24 @@
         static void Main(string[] args)
         {
             EqualPairsSolution equalPairsSolution = new EqualPairsSolution();
-            int[][] grid = new int[][]
+            //int[][] grid = new int[][]
+            //{
+            //    new int[] { 3, 2, 1 },
+            //    new int[] { 1, 7, 6 },
+            //    new int[] { 2, 7, 7 }
+            //};
+            //int result = equalPairsSolution.EqualPairs(grid);
+            //Console.WriteLine("Case 1: " + result); // Expected output: 1
+
+            int[][] grid2 = new int[][]
             {
-                new int[] { 3, 2, 1 },
-                new int[] { 1, 7, 6 },
-                new int[] { 2, 7, 7 }
+                new int[] {3, 1, 2, 2},
+                new int[] {1, 4, 4, 5},
+                new int[] {2, 4, 2, 2},
+                new int[] {2, 4, 2, 2 }
             };
-            int result = equalPairsSolution.EqualPairs(grid);
-            Console.WriteLine(result); // Expected output: 2
+            int result2 = equalPairsSolution.EqualPairs(grid2);
+            Console.WriteLine("Case 2: " + result2); // Expected output: 3
         }
     }
 }
