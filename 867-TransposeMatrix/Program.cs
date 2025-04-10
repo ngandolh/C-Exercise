4eb13ba8e070b6f7f7f0bb635e54e4f5209d5ddc
ngandolh <nganttk002@gmail.com>
2025-04-10 22:17:54 +0700

    [Done]

---
+++ 867-TransposeMatrix/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _867_TransposeMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TransposeSolution transposeSolution = new TransposeSolution();
+            int[][] matrix = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 },
+                new int[] { 7, 8, 9 }
+            };
+            int[][] transposedMatrix = transposeSolution.Transpose(matrix);
+            Console.WriteLine("Case 1: ");
+            foreach (var row in transposedMatrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+
+            Console.WriteLine("==========================");
+
+            int[][] matrix2 = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 }
+            };
+            int[][] transposedMatrix2 = transposeSolution.Transpose(matrix2);
+            Console.WriteLine("Case 2: ");
+            //foreach (var row in transposedMatrix2)
+            //{
+            //    Console.WriteLine(string.Join(" ", row));
+            //}
+        }
+    }
+}
---
+++ 867-TransposeMatrix/TransposeSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _867_TransposeMatrix
+{
+    internal class TransposeSolution
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            int rowCount = matrix.Length;
+            int colCount = matrix[0].Length;
+            int[][] result = new int[colCount][];
+
+            for (int i = 0; i < colCount; i++)
+            {
+                result[i] = new int[rowCount];
+                for (int j = 0; j < rowCount; j++)
+                {
+                    result[i][j] = matrix[j][i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
