723155e33d4bac2e38fbeb36de7b9a5f3278d850
ngandolh <nganttk002@gmail.com>
2025-04-10 13:13:58 +0700

    [Done]

---
+++ 1207-UniqueNumberOccurrences/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1207_UniqueNumberOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueOccurrencesSolution uniqueOccurrencesSolution = new UniqueOccurrencesSolution();
+            int[] arr = { 1, 2, 2, 1, 1, 3 };
+            bool result = uniqueOccurrencesSolution.UniqueOccurrences(arr);
+            Console.WriteLine(result); // Output: true
+
+            int[] arr2 = { 1, 2 };
+            bool result2 = uniqueOccurrencesSolution.UniqueOccurrences(arr2);
+            Console.WriteLine(result2); // Output: false
+
+            int[] arr3 = {-3, 0, 1, -3, 1, 1,1 , -3, 10, 0 };
+            bool result3 = uniqueOccurrencesSolution.UniqueOccurrences(arr3);
+            Console.WriteLine(result3); // Output: true
+
+        }
+    }
+}
---
+++ 1207-UniqueNumberOccurrences/UniqueOccurrencesSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1207_UniqueNumberOccurrences
+{
+    internal class UniqueOccurrencesSolution
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            bool result = true;
+            Dictionary<int, int> uniqueNumers = new Dictionary<int, int>();
+            foreach (int num in arr)
+            {
+                if (uniqueNumers.ContainsKey(num))
+                {
+                    uniqueNumers[num]++;
+                } else
+                {
+                    uniqueNumers.Add(num, 1);
+                }
+            }
+
+            // Check if the occurrences are unique
+            HashSet<int> occurrences = new HashSet<int>();
+            foreach (int count in uniqueNumers.Values)
+            {
+                if (!occurrences.Add(count))
+                {
+                    result = false; 
+                }
+            }
+
+            return result;
+        }
+    }
+}
