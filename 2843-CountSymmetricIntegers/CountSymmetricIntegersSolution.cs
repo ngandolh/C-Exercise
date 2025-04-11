bd96046688fd268343a596a9989193df3bcd335c
ngandolh <nganttk002@gmail.com>
2025-04-11 10:29:16 +0700

    [Done]

---
+++ 2843-CountSymmetricIntegers/CountSymmetricIntegersSolution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2843_CountSymmetricIntegers
+{
+    internal class CountSymmetricIntegersSolution
+    {
+        public int CountSymmetricIntegers(int low, int high)
+        {
+            int count = 0;
+            for (int x = low; x <= high; x++)
+            {
+                string s = x.ToString();
+
+                // Check if the length of the number is even
+                if (s.Length % 2 != 0)
+                {
+                    continue;
+                }
+
+                int n = s.Length / 2;
+
+                int sum1 = 0, sum2 = 0;
+
+                // Calculate the sum of the first half and the second half
+                for (int i = 0; i < n; i++)
+                {
+                    sum1 += s[i] - '0';
+                }
+
+                for (int i = n; i < 2 * n; i++)
+                {
+                    sum2 += s[i] - '0';
+                }
+
+                // Check if the two sums are equal
+                if (sum1 == sum2)
+                {
+                    count++;
+                }
+            }
+
+            return count++;
+        }
+    }
+}
---
+++ 2843-CountSymmetricIntegers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2843_CountSymmetricIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountSymmetricIntegersSolution solution = new CountSymmetricIntegersSolution();
+            int low = 1;
+            int high = 1000;
+            int result = solution.CountSymmetricIntegers(low, high);
+            Console.WriteLine($"Count of symmetric integers between {low} and {high}: {result}"); //Expected output: 9
+
+            // Test with other ranges
+            int low2 = 1200;
+            int high2 = 1230;
+            int result2 = solution.CountSymmetricIntegers(low2, high2);
+            Console.WriteLine($"Count of symmetric integers between {low2} and {high2}: {result2}"); //Expected output: 4   
+        }
+    }
+}
