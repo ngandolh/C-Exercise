712c64abf3e9483e0efb21df909de0bee462c114
ngandolh <nganttk002@gmail.com>
2025-04-23 10:35:13 +0700

    [Done]

---
+++ 1399-CountLargestGroup/CountLargestGroupSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1399_CountLargestGroup
+{
+    internal class CountLargestGroupSolution
+    {
+        public int CountLargestGroup(int n)
+        {
+            //Save the size of each group in a dictionary
+            //The key is the digit sum, and the value is the size of the group
+            Dictionary<int, int> groupSizes = new Dictionary<int, int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = GetDigitSum(i);
+                if (groupSizes.ContainsKey(sum))
+                {
+                    groupSizes[sum]++;
+                }
+                else
+                {
+                    groupSizes[sum] = 1;
+                }
+            }
+
+            int maxSize = groupSizes.Values.Max();
+            int count = groupSizes.Values.Count(size => size == maxSize);
+
+            return count;
+        }
+
+        private int GetDigitSum(int num)
+        {
+            int sum = 0;
+            while(num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ 1399-CountLargestGroup/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1399_CountLargestGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountLargestGroupSolution countLargestGroupSolution = new CountLargestGroupSolution();
+            int n1 = 13;
+            int result1 = countLargestGroupSolution.CountLargestGroup(n1);
+            Console.WriteLine($"Case 1: ");
+            Console.WriteLine($"Input: {n1}, Output: {result1}");
+        }
+    }
+}
