be581ff5e8ee8f6294782ad3c1269ec7208a4fb3
ngandolh <nganttk002@gmail.com>
2025-04-24 16:12:35 +0700

    [Done] Maxium Sum Subarray Size K

---
+++ SlidingWindow/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SlidingWindow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumSumSubarraySizeKSolution maximumSumSubarraySizeKSolution = new MaximumSumSubarraySizeKSolution();
+            int[] nums = { 2, 1, 5, 1, 3, 2 };
+            int k = 2;
+            int result = maximumSumSubarraySizeKSolution.MaximumSumSubarraySize(nums, k);
+            Console.WriteLine($"Maximum sum of subarray of size {k} is: {result}");
+        }
+    }
+}
