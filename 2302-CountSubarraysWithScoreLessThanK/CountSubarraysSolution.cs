46781292a7ea902f3d659b7bb36428d228e8b438
ngandolh <nganttk002@gmail.com>
2025-04-28 17:25:51 +0700

    [Done]

---
+++ 2302-CountSubarraysWithScoreLessThanK/CountSubarraysSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2302_CountSubarraysWithScoreLessThanK
+{
+    internal class CountSubarraysSolution
+    {
+        public long CountSubarrays(int[] nums, long k)
+        {
+            //Approve subarray
+            long result = 0;
+            int n = nums.Length;
+            int start = 0;
+            long windowSum = 0;
+
+            for(int end = 0; end < n; end++)
+            {
+                windowSum += nums[end];
+                //if (windowSum * count < k)
+                //{
+                //    result++;
+                //}
+
+                //When it's satisfy the conditions , we need to shrink the window from the left side
+                while (start <= end && windowSum * (end - start + 1) >= k)
+                {
+                    windowSum -= nums[start];
+                    start++;
+                }
+
+                result += (end - start + 1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 2302-CountSubarraysWithScoreLessThanK/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2302_CountSubarraysWithScoreLessThanK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountSubarraysSolution countSubarraysSolution = new CountSubarraysSolution();
+            int[] nums1 = { 2, 1, 4, 3, 5 };
+            long k1 = 10;
+            long result1 = countSubarraysSolution.CountSubarrays(nums1, k1);
+            Console.WriteLine($"Result for nums1: {result1}"); // Expected output: 6
+        }
+    }
+}
