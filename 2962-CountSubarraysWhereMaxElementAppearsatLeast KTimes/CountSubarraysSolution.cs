05909bae83710476968bac04c1a0253aee37d507
ngandolh <nganttk002@gmail.com>
2025-04-29 22:28:57 +0700

    [Done]

---
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeast KTimes/CountSubarraysSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2962_CountSubarraysWhereMaxElementAppearsatLeast_KTimes
+{
+    internal class CountSubarraysSolution
+    {
+        public long CountSubarrays(int[] nums, int k)
+        {
+            int start = 0, countMax = 0;
+            long result = 0;
+
+            //1. Define the max value in the current window
+            int maxValue = nums.Max();
+
+            for (int end = 0; end < nums.Length; end++)
+            {
+                //2. Check if the current number is the max value
+                if (nums[end] == maxValue)
+                {
+                    countMax++;
+                }
+
+                while (countMax >= k)
+                {
+                    //3. If the count of max value is greater than or equal to k, we can count the subarrays
+                    result += nums.Length - end;
+
+                    //4. Move the start pointer to the right
+                    if (nums[start] == maxValue)
+                    {
+                        countMax--;
+                    }
+                    start++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 2962-CountSubarraysWhereMaxElementAppearsatLeast KTimes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2962_CountSubarraysWhereMaxElementAppearsatLeast_KTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountSubarraysSolution countSubarraysSolution = new CountSubarraysSolution();
+            int[] nums = { 1, 3, 2, 3, 3 };
+            int k1 = 2;
+            long result1 = countSubarraysSolution.CountSubarrays(nums, k1);
+            Console.WriteLine($"Result for k={k1}: {result1}"); // Expected output: 6
+        }
+    }
+}
