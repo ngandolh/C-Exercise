34ba2eddb1fd91c12774a0d635e9c0020847cdda
ngandolh <nganttk002@gmail.com>
2025-04-30 20:50:52 +0700

    [Done]

---
+++ 1295-FindNumbersEvenNumberDigits/FindNumbersSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1295_FindNumbersEvenNumberDigits
+{
+    internal class FindNumbersSolution
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int count = 0;
+            foreach(int num in nums)
+            {
+                if (num.ToString().Length % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 1295-FindNumbersEvenNumberDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1295_FindNumbersEvenNumberDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindNumbersSolution findNumbersSolution = new FindNumbersSolution();
+            int[] nums = { 12, 345, 2, 6, 7896 };
+            int result = findNumbersSolution.FindNumbers(nums);
+            Console.WriteLine($"The count of numbers with even number of digits is: {result}"); //Output: 2
+
+            // Test case 2
+            int[] nums2 = { 555, 901, 482, 1771 };
+            int result2 = findNumbersSolution.FindNumbers(nums2);
+            Console.WriteLine($"The count of numbers with even number of digits is: {result2}"); //Output: 1
+
+            int[] nums3 = { 1, 2, 3, 4, 5 };
+            int result3 = findNumbersSolution.FindNumbers(nums3);
+            Console.WriteLine($"The count of numbers with even number of digits is: {result3}"); //Output: 0
+        }
+    }
+}
