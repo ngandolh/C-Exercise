7b6ca89d35d4ffe0d85f9d77b821f87d02839b18
ngandolh <nganttk002@gmail.com>
2025-05-04 12:24:17 +0700

    [Done]

---
+++ 1128-NumberEquivalentDominoPairs/NumEquivDominoPairsSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1128_NumberEquivalentDominoPairs
+{
+    internal class NumEquivDominoPairsSolution
+    {
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            var map = new Dictionary<(int, int), int>();
+            int count = 0;
+
+            foreach (var d in dominoes)
+            {
+                int a = Math.Max(d[0], d[1]);
+                int b = Math.Min(d[0], d[1]);
+                var key = (a, b);
+
+                if (map.ContainsKey(key))
+                {
+                    count += map[key];
+                    map[key]++;
+                    Console.WriteLine($"Key: {key}, Count: {map[key]}"); 
+                }
+                else
+                {
+                    map[key] = 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 1128-NumberEquivalentDominoPairs/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _1128_NumberEquivalentDominoPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumEquivDominoPairsSolution numEquivDominoPairsSolution = new NumEquivDominoPairsSolution();
+            int[][] dominoes = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 2, 1 },
+                new int[] { 3, 4 },
+                new int[] { 5, 6 }
+            };
+            int result = numEquivDominoPairsSolution.NumEquivDominoPairs(dominoes);
+            Console.WriteLine($"Case 1: " + result); // Output: 1
+        }
+    }
+}
