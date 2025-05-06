0428938245171fd335a22671cebfda6e8c8045e7
ngandolh <nganttk002@gmail.com>
2025-05-06 09:55:03 +0700

    [Done]

---
+++ 1920-BuildArrayfromPermutation/BuildArraySolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1920_BuildArrayfromPermutation
+{
+    internal class BuildArraySolution
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] build = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                build[i] = nums[nums[i]];
+            }
+            return build;
+        }
+    }
+}
---
+++ 1920-BuildArrayfromPermutation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _1920_BuildArrayfromPermutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildArraySolution buildArraySolution = new BuildArraySolution();
+            int[] nums = { 0, 2, 1, 5, 3, 4 };
+            int[] result = buildArraySolution.BuildArray(nums);
+            Console.WriteLine($"Case 1" + string.Join(", ", result)); //Expected: 0, 1, 2, 4, 5, 3
+        }
+    }
+}
