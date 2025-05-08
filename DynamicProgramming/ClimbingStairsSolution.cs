96caa963158c5af5f4b2adad4961993a41b88ea9
ngandolh <nganttk002@gmail.com>
2025-05-08 22:43:29 +0700

    [Done] Climbing Stairs Solution

---
+++ DynamicProgramming/ClimbingStairsSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    internal class ClimbingStairsSolution
+    {
+        //Bạn đang ở chân cầu thang có n bậc.Mỗi lần bạn có thể bước 1 hoặc 2 bậc.Hỏi có bao nhiêu cách để lên tới bậc thứ n?
+        //Input: n = 5
+        //Output: 8
+        //(Vì có 8 cách để đi từ 0 lên 5)
+        //Time Complexity: O(2^n)
+        public int ClimbingStairs(int n)
+        {
+            int result = 0;
+            if (n <= 2) return n;
+
+            //ClimbingStairs(n) = ClimbingStairs(n - 1) + ClimbingStairs(n - 2);
+            return ClimbingStairs(n - 1) + ClimbingStairs(n - 2);
+        }
+    }
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DynamicProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClimbingStairsSolution climbingStairsSolution = new ClimbingStairsSolution();
+            int n1 = 1;
+            int n2 = 2;
+            int n3 = 4;
+
+            climbingStairsSolution.ClimbingStairs(n1);
+            Console.WriteLine("Result for n1: " + climbingStairsSolution.ClimbingStairs(n1));
+
+            climbingStairsSolution.ClimbingStairs(n2);
+            Console.WriteLine("Result for n2: " + climbingStairsSolution.ClimbingStairs(n2));
+
+            climbingStairsSolution.ClimbingStairs(n3);
+            Console.WriteLine("Result for n3: " + climbingStairsSolution.ClimbingStairs(n3));
+        }
+    }
+}
