5054b6331c93bf7e3e1f67e23135871b12d88ccd
ngandolh <nganttk002@gmail.com>
2025-05-08 23:00:50 +0700

    [Done] Fibonacci Number Solution

---
+++ DynamicProgramming/FibonacciNumberSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    internal class FibonacciNumberSolution
+    {
+        public int FibonacciNumber(int n)
+        {
+            if (n <= 1) return n;
+
+            return FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
+        }
+    }
+}
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -4,19 +4,34 @@
     {
         static void Main(string[] args)
         {
-            ClimbingStairsSolution climbingStairsSolution = new ClimbingStairsSolution();
-            int n1 = 1;
-            int n2 = 2;
-            int n3 = 4;
+            //ClimbingStairsSolution climbingStairsSolution = new ClimbingStairsSolution();
+            //int n1 = 1;
+            //int n2 = 2;
+            //int n3 = 4;
 
-            climbingStairsSolution.ClimbingStairs(n1);
-            Console.WriteLine("Result for n1: " + climbingStairsSolution.ClimbingStairs(n1));
+            //climbingStairsSolution.ClimbingStairs(n1);
+            //Console.WriteLine("Result for n1: " + climbingStairsSolution.ClimbingStairs(n1));
 
-            climbingStairsSolution.ClimbingStairs(n2);
-            Console.WriteLine("Result for n2: " + climbingStairsSolution.ClimbingStairs(n2));
+            //climbingStairsSolution.ClimbingStairs(n2);
+            //Console.WriteLine("Result for n2: " + climbingStairsSolution.ClimbingStairs(n2));
 
-            climbingStairsSolution.ClimbingStairs(n3);
-            Console.WriteLine("Result for n3: " + climbingStairsSolution.ClimbingStairs(n3));
+            //climbingStairsSolution.ClimbingStairs(n3);
+            //Console.WriteLine("Result for n3: " + climbingStairsSolution.ClimbingStairs(n3));
+
+            FibonacciNumberSolution fibonacciNumberSolution = new FibonacciNumberSolution();
+            int n4 = 0;
+            int n5 = 1;
+            int n6 = 2;
+            int n7 = 3;
+            
+            fibonacciNumberSolution.FibonacciNumber(n4); //Output: 0
+            Console.WriteLine("Result for n4: " + fibonacciNumberSolution.FibonacciNumber(n4));
+            fibonacciNumberSolution.FibonacciNumber(n5); //Output: 1
+            Console.WriteLine("Result for n5: " + fibonacciNumberSolution.FibonacciNumber(n5));
+            fibonacciNumberSolution.FibonacciNumber(n6); //Output: 1
+            Console.WriteLine("Result for n6: " + fibonacciNumberSolution.FibonacciNumber(n6));
+            fibonacciNumberSolution.FibonacciNumber(n7); //Output: 2
+            Console.WriteLine("Result for n7: " + fibonacciNumberSolution.FibonacciNumber(n7));
         }
     }
 }
