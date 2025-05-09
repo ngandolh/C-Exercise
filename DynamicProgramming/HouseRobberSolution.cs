a788c46697da2e01fa0abd7d43bb211ea8a5221a
ngandolh <nganttk002@gmail.com>
2025-05-09 09:25:30 +0700

    [Done] House Robber Solution

---
+++ DynamicProgramming/HouseRobberSolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramming
+{
+    internal class HouseRobberSolution
+    {
+        public int Rob(int[] nums)
+        {
+            if(nums.Length <= 2)
+            {
+                return nums.Length;
+            }
+
+            int[] dp = new int[nums.Length];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                Console.WriteLine("i: " + i + " dp[i - 1]: " + dp[i - 1] + " dp[i - 2]: " + dp[i - 2] + " nums[i]: " + nums[i]);
+                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+
+            return dp[nums.Length - 1];
+        }
+    }
+}
--- DynamicProgramming/Program.cs
+++ DynamicProgramming/Program.cs
@@ -18,20 +18,35 @@
             //climbingStairsSolution.ClimbingStairs(n3);
             //Console.WriteLine("Result for n3: " + climbingStairsSolution.ClimbingStairs(n3));
 
-            FibonacciNumberSolution fibonacciNumberSolution = new FibonacciNumberSolution();
-            int n4 = 0;
-            int n5 = 1;
-            int n6 = 2;
-            int n7 = 3;
-            
-            fibonacciNumberSolution.FibonacciNumber(n4); //Output: 0
-            Console.WriteLine("Result for n4: " + fibonacciNumberSolution.FibonacciNumber(n4));
-            fibonacciNumberSolution.FibonacciNumber(n5); //Output: 1
-            Console.WriteLine("Result for n5: " + fibonacciNumberSolution.FibonacciNumber(n5));
-            fibonacciNumberSolution.FibonacciNumber(n6); //Output: 1
-            Console.WriteLine("Result for n6: " + fibonacciNumberSolution.FibonacciNumber(n6));
-            fibonacciNumberSolution.FibonacciNumber(n7); //Output: 2
-            Console.WriteLine("Result for n7: " + fibonacciNumberSolution.FibonacciNumber(n7));
+            //FibonacciNumberSolution fibonacciNumberSolution = new FibonacciNumberSolution();
+            //int n4 = 0;
+            //int n5 = 1;
+            //int n6 = 2;
+            //int n7 = 3;
+
+            //fibonacciNumberSolution.FibonacciNumber(n4); //Output: 0
+            //Console.WriteLine("Result for n4: " + fibonacciNumberSolution.FibonacciNumber(n4));
+            //fibonacciNumberSolution.FibonacciNumber(n5); //Output: 1
+            //Console.WriteLine("Result for n5: " + fibonacciNumberSolution.FibonacciNumber(n5));
+            //fibonacciNumberSolution.FibonacciNumber(n6); //Output: 1
+            //Console.WriteLine("Result for n6: " + fibonacciNumberSolution.FibonacciNumber(n6));
+            //fibonacciNumberSolution.FibonacciNumber(n7); //Output: 2 
+            //Console.WriteLine("Result for n7: " + fibonacciNumberSolution.FibonacciNumber(n7));
+
+            HouseRobberSolution houseRobberSolution = new HouseRobberSolution();
+            int[] nums1 = { 2, 7, 9, 3, 1 };
+            int[] nums2 = { 1, 2, 3, 1 };
+            int[] nums3 = { 2, 1, 1, 2 };
+            int[] nums4 = { 1, 2, 3, 4, 5 };
+
+            int result1 = houseRobberSolution.Rob(nums1);
+            Console.WriteLine("Result for nums1: " + result1); //Output: 12
+            //int result2 = houseRobberSolution.Rob(nums2);
+            //Console.WriteLine("Result for nums2: " + result2); //Output: 4
+            //int result3 = houseRobberSolution.Rob(nums3);
+            //Console.WriteLine("Result for nums3: " + result3); //Output: 4
+            //int result4 = houseRobberSolution.Rob(nums4);
+            //Console.WriteLine("Result for nums4: " + result4); //Output: 9
         }
     }
 }
