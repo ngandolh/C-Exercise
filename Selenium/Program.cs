123d3058e289a0a102ee8cf67ae5394eb67cd962
ngandolh <nganttk002@gmail.com>
2025-05-10 23:04:43 +0700

    [Done] Sample 1

---
+++ Selenium/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Selenium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Selenium/SeleniumDemo.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    internal class SeleniumDemo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\TESTER\\SeleniumC#");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void RunTest()
+        {
+            driver.Navigate().GoToUrl("https://anhtester.com/");
+
+            Thread.Sleep(1000);
+            driver.FindElement(By.Id("btn-login")).Click();
+
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys("autotest@mailinator.com");
+
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("123456");
+
+            driver.FindElement(By.Id("login")).Click();
+
+            Thread.Sleep(2000);
+            driver.FindElement(By.LinkText("My Profile")).Click();
+
+            Thread.Sleep(2000);
+        }
+
+        [TearDown]
+        public void CloseTest()
+        {
+            driver.Quit();
+        }
+    }
+}
