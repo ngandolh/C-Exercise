de45f47cc08eca770c4508012dc1a76a2a280ab3
ngandolh <nganttk002@gmail.com>
2025-05-13 23:05:27 +0700

    [Progress]

---
+++ 3335-TotalCharactersStringAfterTransformationsI/LengthAfterTransformationsSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3335_TotalCharactersStringAfterTransformationsI
+{
+    internal class LengthAfterTransformationsSolution
+    {
+        public int LengthAfterTransformations(string s, int t)
+        {
+
+        }
+    }
+}
