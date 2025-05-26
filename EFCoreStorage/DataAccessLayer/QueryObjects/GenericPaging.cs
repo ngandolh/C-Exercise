cc80a491f0643cfcf4d12a3ba548c69555636bf5
ngandolh <nganttk002@gmail.com>
2025-05-26 23:32:43 +0700

    [Add] GenericPaging

---
+++ EFCoreStorage/DataAccessLayer/QueryObjects/GenericPaging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.QueryObjects
+{
+    public static class GenericPaging
+    {
+        public static IQueryable<T> Page<T>(this IQueryable<T> query,
+            int pageNumZeroStart, int pageSize)
+        {
+            if (pageSize == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageSize),
+                    "Page size must be greater than zero.");
+            }
+
+            if(pageNumZeroStart != 0)
+            {
+                query = query
+                    .Skip(pageNumZeroStart * pageSize);
+            }
+
+            return query
+                .Take(pageSize);
+        }
+    }
+}
