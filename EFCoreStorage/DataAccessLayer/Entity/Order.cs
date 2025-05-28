e31cd788a35b68d49cae17fbb273445cc28a0f4b
ngandolh <nganttk002@gmail.com>
2025-05-28 22:31:42 +0700

    [Add] Entity Order

---
+++ EFCoreStorage/DataAccessLayer/Entity/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entity
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime DateOrderedUtc { get; set; }
+        public Guid CustomerId { get; set; }
+
+        // relationships
+
+        //public ICollection<LineItem> LineItems { get; set; }
+
+        // Extra columns not used by EF
+
+        public string OrderNumber => $"SO{OrderId:D6}";
+
+        public Order()
+        {
+            DateOrderedUtc = DateTime.UtcNow;
+        }
+    }
+}
